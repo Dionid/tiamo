0cace6a0ada2b00f6c28345f35bee5c725859c7e
dionid <ditreyw@gmail.com>
2020-07-05 22:40:02 +0300

    Kindof work

--- packages/backend/src/modules/auth/application/events/index.ts
+++
@@ -1,9 +0,0 @@
-import { v4 } from "uuid"
-import { UserId } from "../../domain/aggregates/user/user.id"
-import { IntegrationEvent } from "@dddl/eda"
-
-export class UserRegistered extends IntegrationEvent {
-  constructor(public readonly userId: UserId) {
-    super(v4())
-  }
-}
