50008bfef0e1ac4d421b493ad0805dab9393f385
dionid <ditreyw@gmail.com>
2020-07-05 23:13:28 +0300

    Added cmd

---
+++ packages/backend/src/applications/main-gql/cmd/index.ts
@@ -0,0 +1,113 @@
+import "reflect-metadata"
+import { Container } from "typedi"
+import * as winston from "winston"
+import dotenv from "dotenv"
+import { format } from "winston"
+import { LOGGER_DI_TOKEN } from "@dddl/logger"
+import knex from "knex"
+import {
+  KNEX_CONNECTION_DI_TOKEN,
+  knexSnakeCaseMappers,
+  TX_CONTAINER_DI_TOKEN,
+  TxContainer,
+} from "@dddl/dal-knex"
+import {
+  ASYNC_EVENT_BUS_PROVIDER_DI_TOKEN,
+  EVENT_BUS_DI_TOKEN,
+  EventBusProvider,
+  EventBusPublisher,
+  SYNC_EVENT_BUS_PROVIDER_DI_TOKEN,
+} from "@dddl/eda"
+import { CQ_BUS_DI_TOKEN } from "@dddl/cqrs"
+import { EventBusInMemoryProvider } from "@dddl/eda-inmemory"
+import { CQBus } from "@dddl/cqrs-inmemory"
+import {
+  AsyncEventBusProviderSetMetaDecorator,
+  AsyncEventBusProviderTransactionDecorator,
+  LoggerDecorator,
+  SyncEventBusProviderSetMetaDecorator,
+  SyncEventBusProviderTransactionDecorator,
+  ValidateRequestDecorator,
+} from "@dddl/usecase-decorators"
+import { KnexTransactionDecorator } from "@dddl/usecase-decorators-knex"
+import { RegisterUserPasswordlessCommand } from "../../../../dist/modules/auth/application/commands/register-user-passwordless/command"
+import { RegisterUserPasswordless } from "../../../modules/auth/application/commands/register-user-passwordless"
+
+async function main() {
+  // ENV
+  dotenv.config()
+
+  // ENV const
+  const connectionString = process.env.MAIN_DB_CONNECTION_STRING
+  if (!connectionString) {
+    throw new Error("Env variable 'MAIN_DB_CONNECTION_STRING' is required")
+  }
+  const botId = process.env.BOT_ID
+  if (!botId) {
+    throw new Error("Env variable 'BOT_ID' is required")
+  }
+  const tgAuthToken = process.env.TG_AUTH_TOKEN
+  if (!tgAuthToken) {
+    throw new Error("Env variable 'TG_AUTH_TOKEN' is required")
+  }
+
+  // Logger
+  const logger = winston.createLogger({
+    format: format.combine(
+      format.errors({ stack: true }),
+      format.metadata(),
+      format.json(),
+    ),
+    transports: [new winston.transports.Console()],
+  })
+  Container.set({ id: LOGGER_DI_TOKEN, value: logger, global: true })
+
+  // DB
+  const pg = knex({
+    client: "pg",
+    connection: connectionString,
+    searchPath: ["knex", "public"],
+    ...knexSnakeCaseMappers(),
+  })
+  Container.set({ id: KNEX_CONNECTION_DI_TOKEN, value: pg, global: true })
+
+  // EDA
+  const syncEventBusProvider = new EventBusInMemoryProvider(true, logger)
+  const asyncEventBusProvider = new EventBusInMemoryProvider(false, logger)
+  // const eventBus: IEventBus = new EventBus(syncEventBusProvider, asyncEventBusProvider)
+  Container.set([
+    { id: EVENT_BUS_DI_TOKEN, type: EventBusPublisher },
+    {
+      id: SYNC_EVENT_BUS_PROVIDER_DI_TOKEN,
+      factory: (): EventBusProvider => syncEventBusProvider.fork(),
+    },
+    {
+      id: ASYNC_EVENT_BUS_PROVIDER_DI_TOKEN,
+      factory: (): EventBusProvider => asyncEventBusProvider.fork(),
+    },
+  ])
+
+  // CQRS
+  const cqBus = new CQBus(logger)
+  Container.set([{ id: CQ_BUS_DI_TOKEN, value: cqBus, global: true }])
+
+  // Repos
+  const txContainer = new TxContainer()
+  Container.set({
+    type: TxContainer,
+    id: TX_CONTAINER_DI_TOKEN,
+  })
+
+  // UseCases
+  cqBus.use(LoggerDecorator)
+  cqBus.use(ValidateRequestDecorator)
+  cqBus.use(AsyncEventBusProviderSetMetaDecorator)
+  cqBus.use(AsyncEventBusProviderTransactionDecorator)
+  cqBus.use(KnexTransactionDecorator)
+  cqBus.use(SyncEventBusProviderSetMetaDecorator)
+  cqBus.use(SyncEventBusProviderTransactionDecorator)
+
+  cqBus.subscribe(RegisterUserPasswordlessCommand, RegisterUserPasswordless)
+}
+
+main()
