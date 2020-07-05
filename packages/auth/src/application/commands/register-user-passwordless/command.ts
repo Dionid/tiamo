b9d7247e933f7dc43df9bc4dfbfd4c7d53ffa1b6
dionid <ditreyw@gmail.com>
2020-07-05 20:04:29 +0300

    Prt

--- packages/auth/src/application/commands/register-user-passwordless/command.ts
+++ packages/auth/src/application/commands/register-user-passwordless/command.ts
@@ -1,19 +1,15 @@
-import {Command} from "@dddl/cqrs"
-import {IsEmail, IsUUID} from "class-validator"
-
+import { Command } from "@dddl/cqrs"
+import { IsEmail, IsUUID } from "class-validator"
 
 export class RegisterUserPasswordlessCommand extends Command {
-    @IsEmail()
-    public email: string
-    @IsUUID()
-    public userId: string
+  @IsEmail()
+  public email: string
+  @IsUUID()
+  public userId: string
 
-    constructor(
-        email: string,
-        userId: string
-    ) {
-        super()
-        this.email = email
-        this.userId = userId
-    }
+  constructor(email: string, userId: string) {
+    super()
+    this.email = email
+    this.userId = userId
+  }
 }
--- packages/auth/src/application/events/index.ts
+++ packages/auth/src/application/events/index.ts
@@ -1,9 +1,9 @@
-import {v4} from "uuid"
-import {UserId} from "../../domain/aggregates/user/user.id"
-import {IntegrationEvent} from "@dddl/eda"
+import { v4 } from "uuid"
+import { UserId } from "../../domain/aggregates/user/user.id"
+import { IntegrationEvent } from "@dddl/eda"
 
 export class UserRegistered extends IntegrationEvent {
-    constructor(public readonly userId: UserId) {
-        super(v4())
-    }
+  constructor(public readonly userId: UserId) {
+    super(v4())
+  }
 }
